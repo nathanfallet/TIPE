32682ac76e2eb9598cbbf6dfbdef86f7269e0867
Nathan Fallet <contact@nathanfallet.me>
2021-03-17 10:30:47 +0100

    Ajout du déchiffrement AES en OCaml

--- ocaml/matrices.ml
+++ ocaml/matrices.ml
@@ -17,7 +17,7 @@
 * Ce sont les éléments de nos matrices
 *)
 
-open Polynomes;;
+open Polynomes
 
 (*
 * La matrice utilisée pour faire le produit
@@ -28,7 +28,18 @@ let m = [|
     1;  2;  3;  1;
     1;  1;  2;  3;
     3;  1;  1;  2
-|];;
+|]
+
+(*
+* La matrice inverse pour inverser le
+* produit des colonnes
+*)
+let rm = [|
+    0xe; 0xb; 0xd; 0x9;
+    0x9; 0xe; 0xb; 0xd;
+    0xd; 0x9; 0xe; 0xb;
+    0xb; 0xd; 0x9; 0xe
+|]
 
 (*
 * On défini le produit de deux matrices
@@ -36,9 +47,10 @@ let m = [|
 * du produit pour le mixage des colonnes dont
 * la taille des matrices est fixé
 *)
-let produit_colonne col =
+let produit_colonne col inverse =
     (* On fabrique la colonne de sortie *)
     let resultat = Array.make 4 0 in
+    let used = if inverse then rm else m in
     for i = 0 to 3 do
         (*
         * Chaque coefficient est la somme des
@@ -46,7 +58,7 @@ let produit_colonne col =
         * avec ceux d'une colonne
         *)
         for k = 0 to 3 do
-            resultat.(i) <- (fois m.(i*4 + k) col.(k)) lxor resultat.(i)
+            resultat.(i) <- (fois used.(i*4 + k) col.(k)) lxor resultat.(i)
         done
     done;
-    resultat;;
+    resultat
