2c4bcf59ac75f6b09981afc37aa3fca0cd647179
Nathan Fallet <contact@nathanfallet.me>
2021-03-17 11:07:09 +0100

    Ajout de CBC et OFB

--- ocaml/ciphers.ml
+++ ocaml/ciphers.ml
@@ -27,6 +27,34 @@ let ecb_e entree cle = chiffrer entree cle
 let ecb_d entree cle = dechiffrer entree cle
 
 (*
+* CBC
+*
+* On ajout un vecteur d'initialisation à l'entrée
+* avant de chiffrer avec l'algorithme
+*)
+
+let cbc_e entree cle vi =
+    let xored = Array.map2 (lxor) entree vi in
+    chiffrer xored cle
+
+let cbc_d entree cle vi =
+    let decrypted = dechiffrer entree cle in
+    Array.map2 (lxor) decrypted vi
+
+(*
+* OFB
+*
+* On chiffre un vecteur d'initialisation à l'entrée
+* avant de l'ajouter à l'entrée
+*)
+
+let ofb_e entree cle vi =
+    let s = chiffrer vi cle in
+    Array.map2 (lxor) entree s
+
+let ofb_d = ofb_e
+
+(*
 * CFB
 *
 * On ajoute un vecteur d'initialisation
